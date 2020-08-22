abec07c7543be8b78506406d537493e674e860a1
Nam-Ki-Bok <nkb7714@naver.com>
2020-08-22 21:42:40 +0900

    [2020.08.22]

---
+++ LeetCode/FindNode.py
@@ -0,0 +1,22 @@
+from collections import deque
+
+
+class TreeNode:
+	def __init__(self, x):
+		self.val = x
+		self.left = None
+		self.right = None
+
+class Solution:
+	def getTargetCopy(self, original: TreeNode, cloned: TreeNode, target: TreeNode) -> TreeNode:
+		queue = deque()
+		queue.append(cloned)
+		while queue:
+			node = queue.popleft()
+			if node.val == target.val:
+				return node
+
+			if node.left:
+				queue.append(node.left)
+			if node.right:
+				queue.append(node.right)
