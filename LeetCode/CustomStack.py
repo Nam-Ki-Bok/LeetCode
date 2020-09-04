eef91f621d8564dbd9cd3f5e32ea3acbdf0afa29
Nam-Ki-Bok <nkb7714@naver.com>
2020-09-04 23:29:41 +0900

    [2020.09.04]

---
+++ LeetCode/CustomStack.py
@@ -0,0 +1,31 @@
+class CustomStack:
+
+	def __init__(self, maxSize: int):
+		self.my_maxsize = maxSize
+		self.my_stack = []
+
+	def push(self, x: int) -> None:
+		if len(self.my_stack) < self.my_maxsize:
+			self.my_stack.append(x)
+
+	def pop(self) -> int:
+		if self.my_stack:
+			return self.my_stack.pop()
+		else:
+			return -1
+
+	def increment(self, k: int, val: int) -> None:
+		idx = 0
+		while idx < k and idx < self.my_maxsize:
+			try:
+				self.my_stack[idx] += val
+			except IndexError:
+				break
+			idx += 1
+
+
+# Your CustomStack object will be instantiated and called as such:
+obj = CustomStack(3)
+# obj.push(x)
+# param_2 = obj.pop()
+# obj.increment(k,val)
